306d95063429ffd90e70e63ed6964b8cf507bdfb
Edgar <nomavillagelagos@gmail.com>
2025-09-11 17:45:15 +0200

    fix: update PostHog environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,24 +47,51 @@ export default function RootLayout({
       <head>
         <GoogleAnalytics />
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]);t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-              
-              // Initialize PostHog with hardcoded values
-              window.posthog.init('phc_UfN3a0MHqVk1VzHz2os4M1BHLxzj5JTEdrhzLP3Bj8z', {
-                api_host: 'https://eu.i.posthog.com',
-                person_profiles: 'identified_only',
-                capture_pageview: true,
-                capture_heatmaps: true,
-                session_recording: {
-                  enabled: true,
-                  recordCrossOriginIframes: true
-                }
-              });
-            `,
-          }}
-        />
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(t,e){
+        var o,n,p,r;
+        e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){
+          function g(t,e){
+            var o=e.split(".");
+            2==o.length&&(t=t[o[0]],e=o[1]);
+            t[e]=function(){
+              t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+            }
+          }
+          (p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",
+          (r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);
+          var u=e;
+          for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){
+            var e="posthog";
+            return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e
+          },u.people.toString=function(){
+            return u.toString(1)+".people (stub)"
+          },o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);
+          e._i.push([i,s,a])
+        },e.__SV=1)
+      }(document,window.posthog||[]);
+
+      // Wait for the PostHog library to load before initializing
+      (function pollForPosthog() {
+        if (window.posthog && typeof window.posthog.init === 'function') {
+          window.posthog.init('phc_UfN3a0MHqVk1VzHz2os4M1BHLxzj5JTEdrhzLP3Bj8z', {
+            api_host: 'https://eu.i.posthog.com',
+            person_profiles: 'identified_only',
+            capture_pageview: true,
+            capture_heatmaps: true,
+            session_recording: {
+              enabled: true,
+              recordCrossOriginIframes: true
+            }
+          });
+        } else {
+          setTimeout(pollForPosthog, 50);
+        }
+      })();
+    `,
+  }}
+/>
       </head>
       <body className={`font-sans ${montserrat.variable} ${nunito.variable} ${caveat.variable}`}>
         <GuideModalProvider>
