83d37c98c1584907673c42523bf5bf0d5cff5b91
Eddi <nomavillagelagos@gmail.com>
2025-09-16 23:04:33 +0200

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,17 @@ export default function RootLayout({
             priceRange: "€€",
           })}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-SJK9W4C7');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${montserrat.variable} ${nunito.variable} ${caveat.variable} font-sans antialiased`}>
         <PostHogProvider>
