38bdc0db5336ab19ef04b13c8b63deb3c94ddfa8
Eddi <nomavillagelagos@gmail.com>
2025-10-21 12:54:48 +0100

    yes

--- app/landing-a/page.tsx
+++ app/landing-a/page.tsx
@@ -154,7 +154,7 @@ export default function LandingPage() {
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('/images/fireplace.jpg')",
+            backgroundImage: "url('/images/fireplace.jpeg')",
           }}
         >
           <div className="absolute inset-0 bg-black/40"></div>
--- app/landing-b/page.tsx
+++ app/landing-b/page.tsx
@@ -148,7 +148,7 @@ export default function LandingPage() {
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('/images/beachpeople.jpg')",
+            backgroundImage: "url('/images/beachpeople.jpeg')",
           }}
         >
           <div className="absolute inset-0 bg-black/40"></div>
